9572157c3e3f2564a78b20fd708fffd48abe5b7e
thomas <derivauxthomas@gmail.com>
2023-01-05 00:29:26 +0100

    first depot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<header className="app-header">
+				<nav>
+					<a>Renvoie en haut</a>
+					<ul>
+						<li>lien1</li>
+						<li>lien2</li>
+						<li>lien3</li>
+						<li>lien4</li>
+					</ul>
+				</nav>
+				<h1>Derivaux Thomas</h1>
+				<h1>Developpeur Front-End</h1>
+				<p>Développeur principalement React, j'aime la création de designs innovants, découvrir de nouveaux outils,
+					et surtout travailler en équipe. Vous pouvez me retrouver ici :
+				</p>
+				<div>
+					avatar
+				</div>
+			</header>
+			<section>Compétences principales : </section>
+			<section>Technologies vues</section>
+			<section>Les projets</section>
+		</div>
+	);
 }
 
 export default App;
